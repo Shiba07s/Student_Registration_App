73c75463c578e8bdb4a2fca983c5c2763b0164ad
Ashutosh Samantaray <116368054+Shiba07s@users.noreply.github.com>
2023-08-15 19:02:03 +0530

    Update MetaExApplicationTests.java

--- src/test/java/com/metaex/MetaExApplicationTests.java
+++ src/test/java/com/metaex/MetaExApplicationTests.java
@@ -1,13 +1,13 @@
 package com.metaex;
 
-import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MetaExApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	// @Test
+	// void contextLoads() {
+	// }
 
 }
